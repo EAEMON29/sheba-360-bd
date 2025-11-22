57b0760bfa6a4eb7dd360d29d51877aaae61d60c
EAEMON29 <bdcallingimon@gmail.com>
2025-11-22 14:56:03 +0600

    file add

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+function Navbar() {
+  const link = (
+    <>
+    <li>HOME</li>
+    <li>MENU</li>
+    <li>ABOUT</li>
+    <li>CONTACT</li>
+    </>
+  )
+  return (
+   <>
+   <div className="navbar  shadow-md bg-black/40 backdrop-blur-md text-white fixed top-0 left-0 right-0 z-1000">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+      </div>
+      <ul
+        tabIndex="-1"
+        className="menu menu-sm dropdown-content bg-black/40 backdrop-blur-md rounded-box z-1 mt-3 w-52 p-2 shadow">
+        {link}
+      </ul>
+    </div>
+<div className='flex item center gap-10'>
+      <a className="btn btn-ghost text-xl text-blue-700">SHEBA 360 BD</a>
+      <div className="navbar-center hidden lg:flex">
+    <ul className="flex gap-6">
+      {link}
+    </ul>
+  </div>
+</div>
+  </div>
+  
+  <div className="navbar-end">
+    <a className="btn">Button</a>
+  </div>
+</div>
+   </>
+  )
+}
+
+export default Navbar
